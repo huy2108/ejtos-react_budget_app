7dcecc06e00678a4e283ec6af47cfb64dd0274e4
huy2108 <109428960+huy2108@users.noreply.github.com>
2023-05-12 09:28:30 +0700

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -12,6 +12,17 @@ const ExpenseItem = (props) => {
         });
     };
 
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost:10
+        }
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        });
+    }
+
     const increaseAllocation = (name) => {
         const expense = {
             name: name,
@@ -31,6 +42,7 @@ const ExpenseItem = (props) => {
         <td>{props.name}</td>
         <td><span>{currency}</span>{props.cost}</td>
         <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><button onClick={event=> decreaseAllocation(props.name)}>-</button></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
